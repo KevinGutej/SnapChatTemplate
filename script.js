ed753d4fba453b35481000fa6f49a5e40e10cc78
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-09-18 18:45:07 +0200

    Adding Js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('login-button').addEventListener('click', function() {
+    window.location.href = 'logIn.html';
+});
+
+document.getElementById('signUp-button').addEventListener('click', function() {
+    window.location.href = 'signUp.html';
+});
